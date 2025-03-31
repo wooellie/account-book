5e5f5e3b0b19a503ff5ee48b9a49361b402f9df9
wooellie <138878522+wooellie@users.noreply.github.com>
2025-03-31 09:11:10 +0800

    define a linklist node

---
+++ linklistBasic.java
@@ -0,0 +1,14 @@
+public class linklistBasic{
+    //define the node
+    static class Node{
+      int data;
+      Node next;
+    }
+    //head of the list
+    Node(int data){
+      this.data = data;
+      this.next = null;
+    }
+}
+
+//
